04bf9ddc9c48510cc051a444d1c661878f3c7397
safayilmaz56 <muhammedsafayilmaz56@gmail.com>
2022-09-25 08:48:14 +0300

    sayının asal olup olmadığı
    java kamp

---
+++ sayiAsalMi/src/sayiAsalMi/Main.java
@@ -0,0 +1,34 @@
+package sayiAsalMi;
+ 
+
+public class Main {
+
+	public static void main(String[] args) {
+		int number = 2;
+		boolean isPrime = true;
+		
+		/*if(number == 2) {
+			System.out.println("sayi asaldir");
+		}*/
+		
+		if(number == 1) {
+			System.out.println("sayi asal degildir");
+			return;
+		}
+		if(number < 1) {
+			System.out.println("hatali bir sayi girdiniz");
+		}
+		for(int i=2;i<number;i++) {
+			if(number % i == 0) {
+				isPrime = false;
+			}
+		}
+		if(isPrime) {
+			System.out.println("sayimiz asaldir");
+		}
+		else
+			System.out.println("sayi asal degildir");
+
+	}
+
+}
